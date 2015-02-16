83ac353fc2bd2ae9ea589fd006b0b7ea97ecae46
Georgy Angelov <georgyangelov@gmail.com>
2015-02-16 21:21:34 +0000

    Make changes in row and column sizes update the sheet update time

--- app/models/row_column_state.rb
+++ app/models/row_column_state.rb
@@ -1,7 +1,7 @@
 class RowColumnState < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
 
-  belongs_to :sheet
+  belongs_to :sheet, touch: true
 
   validates_presence_of :index, :type
   validates_numericality_of :index
